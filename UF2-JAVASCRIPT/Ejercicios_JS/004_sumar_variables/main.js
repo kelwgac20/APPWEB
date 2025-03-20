3d11840d304fa4280bec93dc2da25e13faa2e49a
Maximiliano González <kelwgac20@gmail.com>
2025-03-20 19:56:29 +0100

    nuevos

---
+++ UF2-JAVASCRIPT/Ejercicios_JS/004_sumar_variables/main.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("ingrese el primer numero"));
+let num2 = parseInt(prompt("ingrese el segundo numero"));
+let mensaje =`Los numeros introducidos son: ${num1} y ${num2}`;
+document.write(mensaje);
+
+let suma = num1 + num2;
+document.write(`<br> suma: ${suma}`);
---
+++ UF2-JAVASCRIPT/Ejercicios_JS/005_multiplicdor/main.js
@@ -0,0 +1,6 @@
+const multiplicador = 7;
+let perro = parseInt(prompt("ingrese cuantos años tiene su perro"));
+let edadHumana = "La edad humana de tu perro es:" perro * Multiplicador;
+
+    document.write(edadHumana);
+
