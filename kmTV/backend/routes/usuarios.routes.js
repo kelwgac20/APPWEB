486a616da5a54abf3be03e0312b91bb7f2128566
Maximiliano González <kelwgac20@gmail.com>
2025-05-06 17:14:31 +0200

    060525

--- kmTV/backend/routes/usuarios.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registrarUsuario } = require("../controllers/usuarios.controller");
-const verificarToken = require("../middlewares/verificarToken");
-
-router.post("/", verificarToken, registrarUsuario);
-
-module.exports = router;
--- kmTV/postgresSQL/kmtvdb.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  firebase_uid TEXT UNIQUE NOT NULL,
-  email TEXT,
-  username TEXT,
-  is_approved BOOLEAN DEFAULT false,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
